d785889c811a64a03cb917b2e76b1b5db826eea4
Nicolás A. Niño <58050431+Ste1nb0cK@users.noreply.github.com>
2025-02-19 17:27:48 -0500

    First experiments with DifferentialEquations.jl, looking good so far. Next is looking at the driven qubit

--- src/stepbystep.jl
+++ src/stepbystep.jl
@@ -1,3 +1,13 @@
 # Description: implementation of the usual MCW
 
-function rk4step()
+function rk4step(rho::VecOrMat{ComplexF64}, Heff::Matrix{ComplexF64}, dt::Float64)
+end
+        k1 = h * f(t, y)
+        k2 = h * f(t + h/2, y + k1/2)
+        k3 = h * f(t + h/2, y + k2/2)
+        k4 = h * f(t + h, y + k3)
+
+        y_values[:, i+1] .= y + (k1 + 2k2 + 2k3 + k4) / 6
+
+function run_single_stepbystep()
+end
