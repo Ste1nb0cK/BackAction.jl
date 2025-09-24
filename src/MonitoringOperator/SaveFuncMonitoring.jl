21774e152a4e2fbcc8468417dde158e8299d733a
Nicolás A. Niño <58050431+Ste1nb0cK@users.noreply.github.com>
2025-09-24 17:39:53 -0500

    Fixed savefunc not calling the correct cache_phi when updating to the next time

--- src/MonitoringOperator/SaveFuncMonitoring.jl
+++ src/MonitoringOperator/SaveFuncMonitoring.jl
@@ -11,7 +11,9 @@ function _save_func_monitoring(u, integrator, cache_phi, counter, inner)
     affect! = integrator.opts.callback.continuous_callbacks[1].affect!
     cache_state = affect!.cache_state
     auxt = -1im * (integrator.t - affect!.cache_jtime[])
-    cache_phi .= ForwardDiff.derivative(theta -> expv(auxt, affect!.Heff_par(theta), affect!.cache_state), affect!.theta0) + expv(auxt, affect!.Heff, cache_phi)
+    cache_phi .= ForwardDiff.derivative(theta -> expv(auxt, affect!.Heff_par(theta), affect!.cache_state), affect!.theta0) +
+                 expv(auxt, affect!.Heff, affect!.cache_phi)
+
     normalization = 1 / norm(integrator.u)
     lmul!(normalization, cache_phi)
     inner[counter[]] = normalization * dot(cache_phi, integrator.u)
