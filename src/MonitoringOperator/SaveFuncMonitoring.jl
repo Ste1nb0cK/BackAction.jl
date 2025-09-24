c28de54ddc80973683b67525978a6333063ad770
Nicolás A. Niño <58050431+Ste1nb0cK@users.noreply.github.com>
2025-09-24 16:08:02 -0500

    Seemed to have fixed the problem with the intermediate time evaluation

--- src/MonitoringOperator/SaveFuncMonitoring.jl
+++ src/MonitoringOperator/SaveFuncMonitoring.jl
@@ -11,14 +11,11 @@ function _save_func_monitoring(u, integrator, cache_phi, counter, inner)
     affect! = integrator.opts.callback.continuous_callbacks[1].affect!
     cache_state = affect!.cache_state
     auxt = -1im * (integrator.t - affect!.cache_jtime[])
-    # ForwardDiff.derivative!(cache_aux1, theta -> expv(auxt, affect!.Heff_par(theta), affect!.cache_state), affect!.theta0)
-    # cache_phi .= expv(auxt, affect!.Heff, cache_phi)
-    # cache_phi .+= cache_aux1
-    cache_phi .= ForwardDiff.derivative(theta -> expv(auxt, affect!.Heff_par(theta), affect!.cache_state), affect!.theta0) #+ expv(auxt, affect!.Heff, cache_phi)
-    # normalization = 1 / norm(integrator.u)
-    # lmul!(normalization, cache_phi)
-    # inner[counter[]] = normalization * dot(cache_phi, integrator.u)
-    # counter[] += 1
+    cache_phi .= ForwardDiff.derivative(theta -> expv(auxt, affect!.Heff_par(theta), affect!.cache_state), affect!.theta0) + expv(auxt, affect!.Heff, cache_phi)
+    normalization = 1 / norm(integrator.u)
+    lmul!(normalization, cache_phi)
+    inner[counter[]] = normalization * dot(cache_phi, integrator.u)
+    counter[] += 1
 
     u_modified!(integrator, false)
     return nothing
